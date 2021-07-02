5f4bba9f237a085158e4a6748d1bb4c0ad475f43
Aiša Dervanović <59751382+dervanovicaisa@users.noreply.github.com>
2021-07-02 12:00:22 +0200

    Auth

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
-use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\HomeSiteController;
 use App\Http\Controllers\SitePagesController;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,23 +20,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect(app()->getLocale());
+  return redirect(app()->getLocale());
 });
 Route::get('/setLocaleRout/{lang}', function ($lang) {
-    App::setlocale($lang);
-    return redirect(app()->getLocale());
+  App::setlocale($lang);
+  return redirect(app()->getLocale());
 })->name('setLocaleRout');
 
- Route::prefix('{lang}')->middleware('setlocale')->group(function() {
-   Route::get('/', [HomeController::class, 'index']);
-   Route::view('/biografija', 'site.pages.biografija');
-   Route::view('/media', 'site.pages.media');
-   Route::view('/dritanizam', 'site.pages.dritanizam');
-   Route::view('/dashboard', 'site.admin.dashboard');
-
-   
-   Route::get('/blog', [BlogController::class ,'index'])->name('blog');
-
-
+Route::prefix('{lang}')->middleware('setlocale')->group(function () {
+  Route::get('/', [HomeSiteController::class, 'index']);
+  Route::view('/biografija', 'site.pages.biografija');
+  Route::view('/media', 'site.pages.media');
+  Route::view('/dritanizam', 'site.pages.dritanizam');
+  Route::view('/dashboard', 'site.admin.dashboard');
+  Route::get('/blog', [BlogController::class, 'index'])->name('blog');
+  Auth::routes();
+  Route::group(['middleware' => 'admin'], function () {
+    Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
   });
-  
+});
+
